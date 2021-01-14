a25f09cd1609decef9b4108d570f0791e6ddc805
Filippo Ranza <filipporanza@gmail.com>
2021-01-14 21:19:43 +0100

    add some documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+//!
+//! # unfold
+//! 
+//! The [unfold](https://en.wikipedia.org/wiki/Fold_(higher-order_function))
+//! function takes as input a function *f* and an initial *i* value and returns 
+//! a list defined as 
+//! [i, f(i), f(f(i)), ...].
+//! 
+//!   
+//! This library defines ```Unfold``` a struct 
+//! that implements the unfold function as an *endless* 
+//! iterator. 
+//! 
+//!
+//! ## Quick Start
+//! 
+//! To use ```Unfold``` is quite simple. The user calls
+//! the new function providing a function as first argument
+//! and the initial value as second argument. 
+//! An ```Unfold``` instance can be then used as any iterator, 
+//! but don't forget: Unfold never ends, it must be stopped.
+//!
+//! Here an example:
+//! ```
+//! use unfold::Unfold;
+//! 
+//! // Create a vector containing the first 5 numbers from the Fibonacci
+//! // series
+//! let fibonacci_numbers: Vec<u64> = Unfold::new(|(a, b)| (b, a + b), (0, 1))
+//!                                          .map(|(a, _)| a)
+//!                                          .take(5)  //Unfold iterator never stops.
+//!                                          .collect();
+//! assert_eq!(vec![0, 1, 1, 2, 3], fibonacci_numbers);
+//! ```
+//!
+//!
+
+
+/// Define an endless unfold iterator
 pub struct Unfold<T, F>
 where
     F: Fn(T) -> T,
@@ -12,8 +51,16 @@ where
     F: Fn(T) -> T,
     T: Copy,
 {
-    pub fn new(func: F, curr: T) -> Self {
-        Self { func, curr }
+    ///
+    ///Create a new Unfold instance
+    ///```
+    /// use unfold::Unfold;
+    /// //initial value is 100, the function counts backward
+    /// let count_down = Unfold::new(|x| x - 1, 100);
+    ///
+    ///```
+    pub fn new(function: F, init: T) -> Self {
+        Self { func: function, curr: init }
     }
 }
 
